87ae9464b55f344d8be3445c51cad0ab940bc43e
saeidsalari <saeid.salari4@gmail.com>
2021-09-10 20:17:04 +0430

    add font-awesome and material ui

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
